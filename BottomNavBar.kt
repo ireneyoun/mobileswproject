ea7c679bf0126d223a82fc22b61ea27929a72d82
songmin04 <167386894+songmin04@users.noreply.github.com>
2025-12-03 15:02:09 +0900

    Add files via upload

--- BottomNavBar.kt
+++ BottomNavBar.kt
@@ -3,10 +3,17 @@ package com.example.snowtimerapp.ui.components
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CenterFocusStrong
+import androidx.compose.material.icons.filled.Fireplace
 import androidx.compose.material.icons.filled.Group
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.HourglassBottom
+import androidx.compose.material.icons.filled.HourglassEmpty
 import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material.icons.filled.Schedule
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Timer
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -34,7 +41,7 @@ fun BottomNavBar(
         BottomNavItem("main", "홈", Icons.Filled.Home),
         BottomNavItem("search", "검색", Icons.Filled.Search),
         BottomNavItem("group", "그룹", Icons.Filled.Group),
-        BottomNavItem("more", "더보기", Icons.Filled.MoreHoriz)
+        BottomNavItem("settings", "설정", Icons.Filled.Settings)
     )
 
     NavigationBar(
